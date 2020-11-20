a2b9d85f6d95394c240249c2560922e0883313fb
Sloont <colinabel93@gmail.com>
2020-11-20 12:01:48 -0500

    added basic functionality for plotting a point

--- xygraphWIP.java
+++ xygraphWIP.java
@@ -1,11 +1,39 @@
-public class xygraphWIP.java
+public class xygraphWIP
 {
-   public static void main(String[] args)
-   
+   public static void main(String[] args)   
    {
+      int x = -2;
+      int y = 4;
+      boolean flag = true;
+   
       for (int j = 25; j >= -25; j--)
       {
-         if (j == 0)
+         if (j/5.0 == y)
+         {
+            for (int i = -25; i <= 25; i++)
+            {
+               if (i/5 == x && flag)
+               {
+                  flag = false;
+                  int pointer = -5;
+                  
+                  while (pointer < x)
+                  {
+                    
+                     System.out.print("          "); //10x space 
+                     pointer++;
+                  }
+                  
+                  System.out.printf("@ {%s,%s}%n", x, y);
+               }
+               
+            }// end for
+            
+         }//end if
+
+      
+      
+         else if (j == 0)
          {
    
             for (int i = -25; i <= 25; i++)
@@ -25,7 +53,7 @@ public class xygraphWIP.java
          }
          else
          {
-            if (j % 5 == 0)
+            if (j % 5 == 0 && j/5 != y)
             {
                System.out.printf("%51d%n", Math.abs(j/5));
             }
